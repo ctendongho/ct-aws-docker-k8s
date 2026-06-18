48e8e1e889a0b2b078b6456fd474c210d0637b86
Charles Tendongho <ctendongho5@gmail.com>
2026-06-18 04:54:14 +0000

    Add public route table and subnet associations

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -20,6 +20,28 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-rt"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
